4b686ecc439a53944dec07b01a58675e0e96c550
Travis Longoria <longoria.public@gmail.com>
2025-12-08 22:28:57 +0100

    uncomment root index

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
-
-  match "/", to: "health#show", via: [:get, :head]
+  root "posts#index"
 end
